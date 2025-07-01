2c04389345c8e0b859473c35cdf16e0ed9f7a15e
KASHYAPCT <kashyapchandran38@gamil.com>
2025-07-01 11:51:46 +0530

    import removed

--- src/pages/Staff-Customers/Add-customers.jsx
+++ src/pages/Staff-Customers/Add-customers.jsx
@@ -7,7 +7,6 @@ import { useFormik } from "formik";
 import React, { useState, useEffect } from "react";
 import { toast, ToastContainer } from 'react-toastify';
 import Breadcrumbs from "../../components/Common/Breadcrumb";
-import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const FormLayouts = () => {
